cce25bfd90133bfe9cfc36ced0c9a2557784134c
Alex Chew <geminia79@gmail.com>
2017-05-23 23:35:25 +0800

    Gravatar, strong params, sign_up form styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,27 @@ class UsersController < ApplicationController
       def index
       end
 
+      def show
+        @user = User.find(params[:id])
+      end
+
       def new
         @user = User.new
       end
 
       def create
-        @user = User.new
+        @user = User.new(user_params)
         if @user.save
+          flash[:success] = 'User created successfully'
           redirect_to root_path
         else
           render 'new'
       end
     end
-end
+
+    private
+
+    def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
